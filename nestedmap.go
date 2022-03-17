57ded704503504b1450108e7fe072c3281954b2f
ari1021 <o.pe.3.vndt.ku@gmail.com>
2022-03-17 17:52:09 +0900

    feat: add NestedMap and some methods

---
+++ nestedmap.go
@@ -0,0 +1,52 @@
+package nestedmap
+
+type nestedMap[A, B comparable, C any] map[A]map[B]C
+
+// NewNestedMap create pointer of nestedMap.
+func NewNestedMap[A, B comparable, C any]() *nestedMap[A, B, C] {
+	nm := nestedMap[A, B, C](make(map[A]map[B]C))
+	return &nm
+}
+
+// Set sets value to nestedMap.
+func (nm *nestedMap[A, B, C]) Set(k1 A, k2 B, v C) {
+	if nm == nil {
+		return
+	}
+	nm.nilMapCheck(k1)
+	(*nm)[k1][k2] = v
+}
+
+// GetOuter gets map of nestedMap.
+func (nm *nestedMap[A, B, C]) GetOuter(k1 A) (map[B]C, bool) {
+	if nm == nil {
+		return nil, false
+	}
+	if ret, ok := (*nm)[k1]; ok {
+		return ret, ok
+	}
+	return nil, false
+}
+
+// GetInner gets value of nestedMap.
+func (nm *nestedMap[A, B, C]) GetInner(k1 A, k2 B) (C, bool) {
+	var zero C
+	if nm == nil {
+		return zero, false
+	}
+	nm.nilMapCheck(k1)
+	outer, ok := (*nm)[k1]
+	if !ok {
+		return zero, false
+	}
+	if inner, ok := outer[k2]; ok {
+		return inner, true
+	}
+	return zero, false
+}
+
+func (nm *nestedMap[A, B, C]) nilMapCheck(k1 A) {
+	if (*nm)[k1] == nil {
+		(*nm)[k1] = make(map[B]C)
+	}
+}
